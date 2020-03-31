7d2504ea132d93879176f3a0b28e034f0b94b188
AchrafMounir ZORGATI <achrafmounir.zorgati@esprit.tn>
2020-03-31 19:38:18 +0100

    test mobile

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const config = require('config');
-//const db = config.get('mongoURI');
-const db = config.get('mongolocal');
+const db = config.get('mongoURI');
+//const db = config.get('mongolocal');
 
 
 const connectDB = async ()=>{
