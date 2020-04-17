16cc1a7e096e0826ad928a8b931f9c53c06a9f28
AchrafMounir ZORGATI <achrafmounir.zorgati@esprit.tn>
2020-04-17 15:14:37 +0100

    asd

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express();
 
@@ -26,15 +27,7 @@ if(process.env.NODE_ENV === 'production'){
     })
 }
 
-
-
 const PORT =  process.env.PORT || 5000;
 
 app.listen(PORT,()=> console.log(`Server started on port ${PORT}`));
 
-const io = socketIO(app);
-
-io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
-  });
