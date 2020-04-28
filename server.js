ea519211f7bace5c8a10f9d64440ff0dd10e8242
AchrafMounir ZORGATI <achrafmounir.zorgati@esprit.tn>
2020-04-28 16:28:27 +0100

    asd

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const path = require('path')
 
 const app = express();
 
-
-
 connectDB();
 
 app.use(express.json({ extended: false}));
@@ -39,9 +37,3 @@ const PORT =  process.env.PORT || 5000;
 
 app.listen(PORT,()=> console.log(`Server started on port ${PORT}`));
 
-const server = require('http').createServer(app)
-const io = require('socket.io').listen(server)
-io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
-});
