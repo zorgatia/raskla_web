45aa4cb084a3a9999c7bbf5393d6c689f1d4afa9
AchrafMounir ZORGATI <achrafmounir.zorgati@esprit.tn>
2020-05-20 01:55:47 +0100

    asd

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(function(req,res,next){
 	res.header("Access-Control-Allow-Headers","Origin, X Requested-with, Content-Type,Accept");
 	next();
 })
-
+app.use(bodyParser({limit: '50MB'}))
 app.use('/api/user',require('./routes/api/user'))
 app.use('/api/auth',require('./routes/api/auth'))
 app.use('/api/product',require('./routes/api/product'))
