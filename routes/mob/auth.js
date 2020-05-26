5e29cd20de6d93074bc4edb942df3d169d7b68e5
AchrafMounir ZORGATI <achrafmounir.zorgati@esprit.tn>
2020-05-26 11:30:57 +0100

    asd

--- routes/mob/auth.js
+++ routes/mob/auth.js
@@ -14,7 +14,7 @@ const User = require('../../models/User');
 
 router.get('/:id',async(req,res)=>{
     try {
-        const user = await User.findById(req.user.id).select('-password');
+        const user = await User.findById(req.params.id).select('-password');
         res.json(user)
     } catch (err) {
         console.error(err.message);
